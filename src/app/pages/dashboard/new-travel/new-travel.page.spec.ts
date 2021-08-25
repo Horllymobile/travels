3743a20c2a7201985a5833e8daafb204e89c6b79
Abraham Olamide James <horlamidex1@gmail.com>
2021-08-25 16:47:28 +0100

    Fixed jpg image not showing for upload

--- src/app/pages/dashboard/new-travel/new-travel.page.spec.ts
+++ src/app/pages/dashboard/new-travel/new-travel.page.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { ImageCropperModule } from 'ngx-image-cropper';
+import { NewTravelPageRoutingModule } from './new-travel-routing.module';
 
 import { NewTravelPage } from './new-travel.page';
 
@@ -9,8 +12,13 @@ describe('NewTravelPage', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ NewTravelPage ],
-      imports: [IonicModule.forRoot()]
+      declarations: [NewTravelPage],
+      imports: [
+        IonicModule.forRoot(),
+        NewTravelPageRoutingModule,
+        ReactiveFormsModule,
+        ImageCropperModule,
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(NewTravelPage);
